1e96e00bd5e37eaa77fa7ccdb37bfac6a00a9de7
Caleb Naess <naesscaleb@gmail.com>
2017-04-19 22:07:18 -0700

    Added domain search functionality

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Some Alexa Skill 
+# Alexa Domain Search
+An Alexa skill to help search and suggest available domain names.
+
+This repo contains the `voice-interface` directory, which holds the configuration needed to register the Alexa skill with Amazon, and it contains the `src` directory which has the actual source code that will be the API that Alexa will be hitting for her responses.
+
+### Installation
+If you want to set this up with Amazon, register and configure your skill with Amazon using the intents and utterances from the `voice-interface` directory, then add the files from the `src` directory to an AWS Lambda function. Below explains some general steps on how to do that:
+1. Create an Amazon Developer account at https://developer.amazon.com, where you can add a new Alexa skill with the Alexa Skill Kit
+2. While adding the skill, you can use contents of `voice-interface/intent-schema.json` for the Intent Schema, and the `voice-interface/sample-utterances.txt` for the Sample Utterances.
+5. Upload the contents of this `src` directory to an AWS Lambda function and add its ARN in the Alexa skill "Configuration" step, so that this Alexa skill will be set up to talk to that endpoint.
+
+And that's it! You can now test sending some voice commands to your skill in the Test section of the Alexa skill set up, and register your skill with Amazon.
+
+More detailed information about setting up an Alexa skill can be found here: https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/overviews/steps-to-build-a-custom-skill
--- src/index.js
+++ src/index.js
@@ -1,42 +1,106 @@
 'use strict';
-var Alexa = require("alexa-sdk");
+var Alexa = require('alexa-sdk');
+var unirest = require('unirest');
+require('dotenv').config();
+
+var AVAILABLE_STATUS = 'undelegated inactive';
+var FOR_SALE_STATUS = 'active marketed';
+var DOMAINR_URL = 'https://domainr.p.mashape.com/v2/';
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = process.env.APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
 var handlers = {
     'LaunchRequest': function () {
-        this.emit('AskSayHello');
+        this.emit('Explain');
     },
-    'TestIntent': function () {
-        this.emit('SayHello')
+    'DomainIntent': function () {
+        this.emit('FindDomain')
     },
-    'SayHello': function () {
+    'FindDomain': function () {
+        var self = this;
+        var domain = self.event.request.intent.slots.Domain;
 
-        var name = this.event.request.intent.slots.Name;
-        var message = null;
-
-        if (name && name.value) {
-          message = 'Hello, '+name.value+'!';
+        if (!(domain && domain.value)) {
+          self.emit('Explain'); // just explain if no domain is sent
         } else {
-          message = 'Hello!';
-        }
+          // if there's no TLD, then add a default ".com"
+          if (!domain.value.includes('.')) {
+            domain.value = domain.value.concat('.com');
+          }
 
-        this.attributes['speechOutput'] = message;
-        this.emit(':tell', message);
+          // use the domainr API to get the status of the domain
+          mashapeGetRequest('status', domain.value, function(statusResult) {
+              var message = '';
+
+              if (statusResult.body.status.length > 0) {
+                if (statusResult.body.status[0].status == AVAILABLE_STATUS) {
+                  // If it's available, just tell the user and that's it
+                  self.attributes['speechOutput'] = 'The domain ' + domain.value + ' is available!';
+                  self.emit(':tell', message);
+                } else { // Not available
+                  // If it's for sale, add that to the message
+                  var forSaleMessage = (statusResult.body.status[0].status == FOR_SALE_STATUS) ? ', but is for sale' : '';
+                  message = 'The domain ' + domain.value + ' is not available' + forSaleMessage + '.';
+
+                  // Get related domains
+                  mashapeGetRequest('search', domain.value, function(suggestionsResult) {
+                    if (suggestionsResult.body.results.length > 0) {
+                      var statusQueryDomains = [];
+                      for (var i = 0; i < suggestionsResult.body.results.length; i++) {
+                        if (suggestionsResult.body.results[i].domain != domain.value) { // Don't suggest the domain they asked for
+                          statusQueryDomains.push(suggestionsResult.body.results[i].domain);
+                        }
+                      }
+
+                      var statusQueryString = statusQueryDomains.join(encodeURIComponent(','));
+
+                      // Get the status of all related domains
+                      mashapeGetRequest('status', statusQueryString, function(suggestionsStatusResult) {
+                        if (suggestionsStatusResult.body.status.length > 0) {
+                          var suggestedDomains = [];
+                          for (var i = 0; i < suggestionsStatusResult.body.status.length; i++) {
+                            if (suggestionsStatusResult.body.status[i].status == AVAILABLE_STATUS) {
+                              suggestedDomains.push(suggestionsStatusResult.body.status[i].domain);
+                            }
+                          }
+
+                          if (suggestedDomains.length === 0) { // If none of the related domains are available
+                            self.attributes['speechOutput'] = message + ' No available related domains were found.';
+                            self.emit(':tell', self.attributes['speechOutput']);
+                          } else { // At least one domain was found
+                            var suggestionsString = statusQueryDomains.join(', ');
+                            self.attributes['speechOutput'] = message + ' Here is a list of suggested available alternatives: ' + suggestionsString;
+                            self.emit(':tell', self.attributes['speechOutput']);
+                          }
+
+                        } else { // Didn't get statuses for suggested domains
+                          this.emit('Error');
+                        }
+                      });
+                    } else { // No related domains found
+                      this.emit('Error');
+                    }
+                  });
+                }
+              } else { // not found
+                this.emit('Error');
+              }
+          });
+
+        }
     },
-    'AskSayHello': function () {
-        this.attributes['speechOutput'] = 'Should I say hello to you?';
-        this.attributes['repromptSpeech'] = 'Do you want me to say hello to you or what?';
+    'Explain': function () {
+        this.attributes['speechOutput'] = 'You can ask me if any domain is available, and I will tell you if it is. And I\'ll give relevant suggestions if it\'s not.';
+        this.attributes['repromptSpeech'] = 'Ask me something like: "Is caleb.com available?"';
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
     },
     'AMAZON.HelpIntent': function () {
-        this.attributes['speechOutput'] = 'You can say hello and then your name. Or you can say exit...so will you tell me your name?';
-        this.attributes['repromptSpeech'] = 'will you tell me your name?';
-        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
+        this.emit('Explain');
     },
     'AMAZON.RepeatIntent': function () {
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
@@ -51,8 +115,22 @@ var handlers = {
         this.emit(':tell', 'Goodbye.');
     },
     'Unhandled': function () {
-      this.attributes['speechOutput'] = 'You can say hello and then your name. Or you can say exit...so will you tell me your name?';
-      this.attributes['repromptSpeech'] = 'will you tell me your name?';
-      this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
+      this.emit('Explain');
+    },
+    'Error': function () {
+      self.attributes['speechOutput'] = 'There was a problem while searching domains. Please try again.';
+      self.emit(':tell', self.attributes['speechOutput']);
     }
 };
+
+function mashapeGetRequest(type, domain, callback) {
+  // Build URL based on type
+  var url = DOMAINR_URL + type + '?' + (type == 'search' ? 'query=' : 'domain=');
+  // Send request and return results
+  unirest.get(url + domain)
+  .header('X-Mashape-Key', process.env.MASHAPE_KEY)
+  .header('Accept', 'application/json')
+  .end(function(result) {
+    callback(result);
+  });
+}
